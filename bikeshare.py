a9d4c19e08ba9a0cb99a847b3567efab727ca76a
Tonima Ferdous <tferdous27@gmail.com>
2021-02-07 23:29:21 -0500

    refactor: Refactor the code to display 6 lines of raw data
    Refactor the code in bikeshare.py to display 6 lines of raw data instead
    of 5 lines. This refactoring helps the users to receive a detailed
    comparison of data for their chosen filters.
    
    See bikeshare.py file from commit co221e4 for the older version.

--- bikeshare.py
+++ bikeshare.py
@@ -200,15 +200,15 @@ def user_stats(df):
 
 
 def raw_data(df):
-    """Display 5 lines of raw data upon request."""
+    """Display 6 lines of raw data upon request."""
     data = input('\nWould you like to see individual trip data? Please type "yes" or "no".\n').lower()
     if data == 'yes':
         index1 = 0
-        index2 = 5
+        index2 = 6
         while True:
             print(df.iloc[index1: index2])
-            index1 += 5
-            index2 += 5
+            index1 += 6
+            index2 += 6
             more_data = input('\nWould you like to see more data? Please type "yes" or "no".\n').lower()
             if more_data != 'yes':
                 break
