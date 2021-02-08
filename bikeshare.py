7150dfd9f7ca95a4546a42419bced4863e1b48d6
Tonima Ferdous <tferdous27@gmail.com>
2021-02-07 23:15:16 -0500

    docs: Change the document string to define get_filters() function
    Create a comprehensive docstring for the definition of get_filters()
    function and change the older one.
    
    See the bikeshare.py file from commit co221e4 to check the older version.

--- bikeshare.py
+++ bikeshare.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks user to specify a city, month, and day to analyze bike sharing data.
 
     Returns:
         (str) city - name of the city to analyze
